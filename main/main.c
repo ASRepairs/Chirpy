4993c73159ca2efee5ae8cc40f947498c7330162
user <altameemiahmed2000@gmail.com>
2025-03-17 20:26:57 +0100

    added fonts and cinfigured spiffs and partition table

--- main/main.c
+++ main/main.c
@@ -4,48 +4,28 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "esp_spiffs.h"
 
-//RA01S LoRa  and lcd st7789 driver libs
-#include "../components/ra01s/ra01s.h"
+
+//  Uncomment the one you want to use.
+// #define ST7789
+#define SSD1306
+
+#ifdef ST7789
 #include "../components/st7789/st7789.h"
 #include "../components/st7789/fontx.h"
-#include "esp_spiffs.h"
+#endif
+
+#ifdef SSD1306
+#include "../components/ssd1306/ssd1306.h"
+#include "../components/ssd1306/font8x8_basic.h"
+#endif
+
+
+#include "../components/ra01s/ra01s.h"
+
+
 static const char *TAG = "ChirpyMain";
-
-// EU frequency and TX power settings for LoRa.
-#define LORA_FREQUENCY 868000000  // 868 MHz for eu
-#define LORA_TX_POWER  14         //(range -3 to +22 dBm)
-
-// LoRa modulation parameters (placeholders for now, TODO: to adjust later)
-#define LORA_SPREADING_FACTOR 7
-#define LORA_BANDWIDTH        0x08
-#define LORA_CODING_RATE      1      //4/5 coded rate might be represented as 1
-#define LORA_PREAMBLE_LENGTH  8
-#define LORA_PAYLOAD_LENGTH   0      // 0 = variable length packet
-#define LORA_CRC_ON           true
-#define LORA_INVERT_IQ        false
-
-// lcd configuration
-#define PIN_MOSI    19
-#define PIN_SCLK    18
-#define PIN_CS      5
-#define PIN_DC      27
-#define PIN_BL      12
-
-#define PIN_RESET   17
-
-#define DISP_WIDTH  240
-#define DISP_HEIGHT 240
-#define COLOR_BLACK 0x0000
-#define COLOR_WHITE 0xFFFF
-
-
-TFT_t tft;
-FontxFile fx16G[2];
-// GPIO configuration for the user button.
-#define BUTTON_GPIO         GPIO_NUM_0  //the en button //GPIO_NUM_36 for the watch button
-#define DEBOUNCE_DELAY_MS   50
-
 
 esp_err_t mountSPIFFS(const char *base_path, const char *partition_label, int max_files)
 {
@@ -68,6 +48,23 @@ esp_err_t mountSPIFFS(const char *base_path, const char *partition_label, int ma
     return ret;
 }
 
+#ifdef ST7789
+
+// ST7789 display configuration and global variables
+#define PIN_MOSI    23
+#define PIN_SCLK    18
+#define PIN_CS      5
+#define PIN_DC      16
+#define PIN_RESET   17
+#define PIN_BL      4
+#define DISP_WIDTH  240
+#define DISP_HEIGHT 240
+#define COLOR_BLACK 0x0000
+#define COLOR_WHITE 0xFFFF
+
+TFT_t tft;
+FontxFile fx16G[2];
+
 void display_init(void)
 {
     spi_master_init(&tft, PIN_MOSI, PIN_SCLK, PIN_CS, PIN_DC, PIN_RESET, PIN_BL);
@@ -87,11 +84,62 @@ void display_print(const char *msg)
 {
     // clear the screen and print the message.
     lcdFillScreen(&tft, COLOR_BLACK);
-    lcdDrawString(&tft, fx16G, 10, 100, (uint8_t *)msg, COLOR_WHITE);//TODO: adjust coords but it should be ok for now
+    lcdDrawString(&tft, fx16G, 10, 100, (uint8_t *)msg, COLOR_WHITE);
 }
 
+#endif // ST7789
 
-// Task to poll for LoRa messages (since RA01S does not use callbacks)
+#ifdef SSD1306
+
+SSD1306_t oled;
+
+void display_init(void)
+{
+
+    i2c_master_init(&oled, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+#ifdef CONFIG_SSD1306_128x64
+    ssd1306_init(&oled, 128, 64);
+#else
+    ssd1306_init(&oled, 128, 32);
+#endif
+    ssd1306_clear_screen(&oled, false);
+    ssd1306_contrast(&oled, 0xff);
+}
+
+void display_clear(void)
+{
+    ssd1306_clear_screen(&oled, false);
+}
+
+void display_print(const char *msg)
+{
+    // Clear the screen and display the text on the first row.
+    ssd1306_clear_screen(&oled, false);
+    ssd1306_display_text(&oled, 0, msg, strlen(msg), false);
+}
+
+#endif // SSD1306
+
+
+
+// LoRa configuration parameters.
+#define LORA_FREQUENCY 868000000  // 868 MHz for EU
+#define LORA_TX_POWER  14         // (range -3 to +22 dBm)
+#define LORA_SPREADING_FACTOR 7
+#define LORA_BANDWIDTH        0x08
+#define LORA_CODING_RATE      1
+#define LORA_PREAMBLE_LENGTH  8
+#define LORA_PAYLOAD_LENGTH   0      // 0 = variable length packet
+#define LORA_CRC_ON           true
+#define LORA_INVERT_IQ        false
+
+// GPIO configuration for the user button.
+#define BUTTON_GPIO         GPIO_NUM_0  // the EN button
+#define DEBOUNCE_DELAY_MS   50
+
+//-------------------------------------------------------------------
+// Task to poll for LoRa messages (polling version as in your code)
+//-------------------------------------------------------------------
 void lora_receive_task(void *arg)
 {
     uint8_t rxBuffer[256];
@@ -136,8 +184,12 @@ void app_main(void)
     ESP_LOGI(TAG, "Mounting SPIFFS for fonts");
     ESP_ERROR_CHECK(mountSPIFFS("/fonts", "storage", 7));
 
-    InitFontx(fx16G, "/fonts/ILGH16XB.FNT", ""); //example font from repo
-    // button GPIO init
+#ifdef ST7789
+    // Initialize font for ST7789 display.
+    InitFontx(fx16G, "/fonts/ILGH16XB.FNT", "");
+#endif
+
+    // Button GPIO initialization.
     gpio_config_t btn_conf = {
         .pin_bit_mask = (1ULL << BUTTON_GPIO),
         .mode = GPIO_MODE_INPUT,
@@ -159,7 +211,9 @@ void app_main(void)
         display_print("LoRa Init Failed");
         return;
     }
-
+    ESP_LOGE(TAG, "LoRaBegin success");
+    display_clear();
+    display_print("LoRa Init success");
     // modulation parameters config
     LoRaConfig(LORA_SPREADING_FACTOR, LORA_BANDWIDTH, LORA_CODING_RATE,
                LORA_PREAMBLE_LENGTH, LORA_PAYLOAD_LENGTH, LORA_CRC_ON, LORA_INVERT_IQ);
@@ -177,6 +231,8 @@ void app_main(void)
             vTaskDelay(pdMS_TO_TICKS(DEBOUNCE_DELAY_MS));
             if (gpio_get_level(BUTTON_GPIO) == 0) {
                 send_lora_message();
+                display_print("message sent");
+                ESP_LOGI(TAG, "lora message sent!");
                 while (gpio_get_level(BUTTON_GPIO) == 0) {
                     vTaskDelay(pdMS_TO_TICKS(10));
                 }
